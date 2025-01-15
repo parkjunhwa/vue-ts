c796e9ed94adf9b7199debdfd15febd3cf4f6fa9
Park Junhwa <junhwa.park@gmail.com>
2025-01-15 09:44:32 +0900

    최초초

---
+++ src/layouts/components/Footer.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="h-100 d-flex align-center justify-center">
+    <span class="d-flex align-center text-medium-emphasis">
+      &copy;
+      {{ new Date().getFullYear() }}
+      명인제약 by UBCare All Right Reserved.
+    </span>
+  </div>
+</template>
