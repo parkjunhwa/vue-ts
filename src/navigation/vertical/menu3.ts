3e40d7cd18cda64b95ea49c494004f467387a789
Park Junhwa <junhwa94@ubcare.co.kr>
2025-01-20 15:57:19 +0900

    tnwjd

--- src/navigation/vertical/menu3.ts
+++ src/navigation/vertical/menu3.ts
@@ -1,13 +1,111 @@
 export default [
-  { heading: 'Menu' },
+  { heading: 'Components' },
   {
-    title: '컴포넌트 모음',
-    icon: { icon: 'tabler-hand-finger' },
+    title: 'Typography',
+    icon: { icon: 'tabler-typography' },
+    to: 'pages-typography',
+  },
+  {
+    title: 'Icons',
+    icon: { icon: 'tabler-brand-tabler' },
+    to: 'pages-icons',
+  },
+  {
+    title: 'Components',
+    icon: { icon: 'tabler-toggle-left' },
+    children: [
+      { title: 'Alert', to: 'components-alert' },
+      { title: 'Avatar', to: 'components-avatar' },
+      { title: 'Badge', to: 'components-badge' },
+      { title: 'Button', to: 'components-button' },
+      { title: 'Chip', to: 'components-chip' },
+      { title: 'Dialog', to: 'components-dialog' },
+      { title: 'Expansion Panel', to: 'components-expansion-panel' },
+      { title: 'List', to: 'components-list' },
+      { title: 'Menu', to: 'components-menu' },
+      { title: 'Pagination', to: 'components-pagination' },
+      { title: 'Progress Circular', to: 'components-progress-circular' },
+      { title: 'Progress Linear', to: 'components-progress-linear' },
+      { title: 'Snackbar', to: 'components-snackbar' },
+      { title: 'Tabs', to: 'components-tabs' },
+      { title: 'Timeline', to: 'components-timeline' },
+      { title: 'Tooltip', to: 'components-tooltip' },
+    ],
+  },
+  {
+    title: 'Form Elements',
+    icon: { icon: 'tabler-copy' },
     children: [
       {
-        title: 'typography',
-        to: 'pages-components',
+        title: 'Autocomplete',
+        to: 'forms-autocomplete',
+      },
+      {
+        title: 'Checkbox',
+        to: 'forms-checkbox',
+      },
+      {
+        title: 'Combobox',
+        to: 'forms-combobox',
+      },
+      {
+        title: 'Date Time Picker',
+        to: 'forms-date-time-picker',
+      },
+      {
+        title: 'Editors',
+        to: 'forms-editors',
+      },
+      {
+        title: 'File Input',
+        to: 'forms-file-input',
+      },
+      {
+        title: 'Radio',
+        to: 'forms-radio',
+      },
+      {
+        title: 'Custom Input',
+        to: 'forms-custom-input',
+      },
+      {
+        title: 'Range Slider',
+        to: 'forms-range-slider',
+      },
+      {
+        title: 'Rating',
+        to: 'forms-rating',
+      },
+      {
+        title: 'Select',
+        to: 'forms-select',
+      },
+      { title: 'Slider', to: 'forms-slider' },
+      {
+        title: 'Switch',
+        to: 'forms-switch',
+      },
+      {
+        title: 'Textarea',
+        to: 'forms-textarea',
+      },
+      {
+        title: 'Textfield',
+        to: 'forms-textfield',
       },
     ],
+  },
+  {
+    title: 'Tables',
+    icon: { icon: 'tabler-layout-grid' },
+    children: [
+      { title: 'Simple Table', icon: { icon: 'tabler-table' }, to: 'tables-simple-table' },
+      { title: 'Data Table', icon: { icon: 'tabler-layout-grid' }, to: 'tables-data-table' },
+    ],
+  },
+  {
+    title: 'Dialog Examples',
+    icon: { icon: 'tabler-square' },
+    to: 'pages-dialog-examples',
   },
 ]
--- src/pages/pages/home.vue
+++ src/pages/pages/home.vue
@@ -1,9 +1,32 @@
 <script setup lang="ts">
-import TypographyHeadlines from '@/views/pages/typography/TypographyHeadlines.vue'
-import TypographyTexts from '@/views/pages/typography/TypographyTexts.vue'
+import TypographyHeadlines from "@/views/pages/typography/TypographyHeadlines.vue";
+import TypographyTexts from "@/views/pages/typography/TypographyTexts.vue";
 </script>
 
 <template>
+  <div class="d-flex flex-wrap justify-start mb-2">
+    <VIcon size="18" icon="tabler-smart-home" />
+    <VBreadcrumbs
+      class="px-0 pb-2 pt-0 help-center-breadcrumbs"
+      :items="[
+        { title: 'Home', to: { name: 'pages-home' }, class: 'text-primary' },
+        { title: '제목' },
+      ]"
+    />
+  </div>
+  <div
+    class="d-flex flex-wrap justify-start justify-sm-space-between gap-y-4 gap-x-6 mb-6"
+  >
+    <div class="d-flex flex-column justify-center">
+      <h4 class="text-h4 font-weight-medium">제목</h4>
+    </div>
+
+    <div class="d-flex gap-4 align-center flex-wrap">
+      <VBtn color="secondary">취소</VBtn>
+      <VBtn color="primary">저장</VBtn>
+    </div>
+  </div>
+
   <VRow>
     <VCol cols="12">
       <TypographyHeadlines />
