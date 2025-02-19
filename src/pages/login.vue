625d13f9095528b00a9ccf0b20b15c604d082897
Park Junhwa <junhwa94@ubcare.co.kr>
2025-02-19 09:44:06 +0900

    로그인 수정정

--- src/pages/login.vue
+++ src/pages/login.vue
@@ -1,40 +1,18 @@
 <!-- ❗Errors in the form are set on line 60 -->
 <script setup lang="ts">
-import AuthProvider from "@/views/pages/authentication/AuthProvider.vue";
-import { useGenerateImageVariant } from "@core/composable/useGenerateImageVariant";
-import authV2LoginIllustrationBorderedDark from "@images/pages/auth-v2-login-illustration-bordered-dark.png";
-import authV2LoginIllustrationBorderedLight from "@images/pages/auth-v2-login-illustration-bordered-light.png";
-import authV2LoginIllustrationDark from "@images/pages/auth-v2-login-illustration-dark.png";
-import authV2LoginIllustrationLight from "@images/pages/auth-v2-login-illustration-light.png";
-import authV2MaskDark from "@images/pages/misc-mask-dark.png";
-import authV2MaskLight from "@images/pages/misc-mask-light.png";
-import { VNodeRenderer } from "@layouts/components/VNodeRenderer";
+import logoIcon from "@images/logos/logo.png";
 import { themeConfig } from "@themeConfig";
 import { VForm } from "vuetify/components/VForm";
-
-const authThemeImg = useGenerateImageVariant(
-  authV2LoginIllustrationLight,
-  authV2LoginIllustrationDark,
-  authV2LoginIllustrationBorderedLight,
-  authV2LoginIllustrationBorderedDark,
-  true
-);
-
-const authThemeMask = useGenerateImageVariant(authV2MaskLight, authV2MaskDark);
-
 definePage({
   meta: {
     layout: "blank",
     unauthenticatedOnly: true,
   },
 });
-
 const isPasswordVisible = ref(false);
-
-const route = useRoute();
-const router = useRouter();
-
-const ability = useAbility();
+// const route = useRoute();
+// const router = useRouter();
+// const ability = useAbility();
 
 const errors = ref<Record<string, string | undefined>>({
   email: undefined,
@@ -86,145 +64,254 @@ const onSubmit = () => {
     if (isValid) login();
   });
 };
+
+// 입력 값 초기화 함수
+const onClear1 = () => {
+  credentials.value.companycode = "";
+};
+const onClear2 = () => {
+  credentials.value.userid = "";
+};
+const onClear3 = () => {
+  credentials.value.password = "";
+};
 </script>
 
 <template>
-  <RouterLink to="/">
-    <div class="auth-logo d-flex align-center gap-x-3">
-      <VNodeRenderer :nodes="themeConfig.app.logo" />
-      <h1 class="auth-title">
-        {{ themeConfig.app.title }}
-      </h1>
-    </div>
-  </RouterLink>
-
-  <VRow no-gutters class="auth-wrapper bg-surface">
-    <VCol md="8" class="d-none d-md-flex">
-      <div class="position-relative bg-background w-100 me-0">
-        <div
-          class="d-flex align-center justify-center w-100 h-100"
-          style="padding-inline: 6.25rem"
-        >
-          <!-- <VImg
-            max-width="613"
-            :src="authThemeImg"
-            class="auth-illustration mt-16 mb-2"
-          /> -->
-        </div>
-
-        <img
-          class="auth-footer-mask"
-          :src="authThemeMask"
-          alt="auth-footer-mask"
-          height="280"
-          width="100"
-        />
-      </div>
-    </VCol>
-
-    <VCol
-      cols="12"
-      md="4"
-      class="auth-card-v2 d-flex align-center justify-center"
-    >
-      <VCard flat :max-width="500" class="mt-12 mt-sm-0 pa-4">
-        <VCardText>
-          <h4 class="text-h4 mb-1">
-            Welcome to
-            <span class="text-capitalize"> {{ themeConfig.app.title }} </span>!
-            👋🏻
-          </h4>
-          <p class="mb-0">
-            Please sign-in to your account and start the adventure
-          </p>
-        </VCardText>
-        <VCardText>
-          <VAlert color="primary" variant="tonal">
-            <p class="text-sm mb-2">
-              Admin Email: <strong>admin@demo.com</strong> / Pass:
-              <strong>admin</strong>
-            </p>
-            <p class="text-sm mb-0">
-              Client Email: <strong>client@demo.com</strong> / Pass:
-              <strong>client</strong>
-            </p>
-          </VAlert>
-        </VCardText>
-        <VCardText>
-          <VForm ref="refVForm" @submit.prevent="onSubmit">
-            <VRow>
-              <!-- email -->
-              <VCol cols="12">
-                <AppTextField
-                  v-model="credentials.email"
-                  label="Email"
-                  placeholder="johndoe@email.com"
-                  type="email"
-                  autofocus
-                  :rules="[requiredValidator, emailValidator]"
-                  :error-messages="errors.email"
-                />
-              </VCol>
-
-              <!-- password -->
-              <VCol cols="12">
-                <AppTextField
-                  v-model="credentials.password"
-                  label="Password"
-                  placeholder="············"
-                  :rules="[requiredValidator]"
-                  :type="isPasswordVisible ? 'text' : 'password'"
-                  autocomplete="password"
-                  :error-messages="errors.password"
-                  :append-inner-icon="
-                    isPasswordVisible ? 'tabler-eye-off' : 'tabler-eye'
-                  "
-                  @click:append-inner="isPasswordVisible = !isPasswordVisible"
-                />
-
-                <div
-                  class="d-flex align-center flex-wrap justify-space-between my-6"
-                >
-                  <VCheckbox v-model="rememberMe" label="Remember me" />
-                  <RouterLink
-                    class="text-primary ms-2 mb-1"
-                    :to="{ name: 'forgot-password' }"
-                  >
-                    Forgot Password?
-                  </RouterLink>
-                </div>
-
-                <VBtn block type="submit"> Login </VBtn>
-              </VCol>
-
-              <!-- create account -->
-              <VCol cols="12" class="text-center">
-                <span>New on our platform?</span>
-                <RouterLink
-                  class="text-primary ms-1"
-                  :to="{ name: 'register' }"
-                >
-                  Create an account
+  <div
+    class="auth-wrapper d-flex align-center justify-center pa-5 background-image"
+  >
+    <div class="position-relative">
+      <VRow class="match-height">
+        <VCol cols="12" sm="12" lg="6" class="d-none d-lg-block">
+          <VCard class="auth-card background-side">
+            <VCardText class="pa-12">
+              <h1 class="text-h1 mb-5 text-white" style="font-weight: 700">
+                <span class="color-red">Better</span><br />
+                Quality medicine<br /><span class="color-red">Better</span
+                ><br />
+                Public Health
+              </h1>
+              <p class="text-white text-h6 mb-0">
+                ‘좋은 치료제, 넘치는 건강’ 명인제약 입니다.
+              </p>
+            </VCardText>
+          </VCard>
+        </VCol>
+        <VCol cols="12" sm="12" lg="6">
+          <VCard class="auth-card">
+            <VCardText class="pa-12">
+              <VCol cols="12" class="pa-0 text-center d-block d-lg-none">
+                <RouterLink to="/" class="d-inline-block pb-6">
+                  <div class="app-logo align-center">
+                    <v-img>
+                      <img :src="logoIcon" alt="Logo" />
+                    </v-img>
+                    <h1 class="app-logo-title">
+                      {{ themeConfig.app.title }}
+                    </h1>
+                  </div>
                 </RouterLink>
+                <h5 class="text-h5 mb-1 text-primary">
+                  <span class="color-blue">Better</span>
+                  Quality medicine<br /><span class="color-blue">Better</span>
+                  Public Health
+                </h5>
+                <p class="text-body-2 mb-10">
+                  ‘좋은 치료제, 넘치는 건강’ 명인제약 입니다.
+                </p>
               </VCol>
-              <VCol cols="12" class="d-flex align-center">
-                <VDivider />
-                <span class="mx-4">or</span>
-                <VDivider />
-              </VCol>
+              <h4 class="text-h4 mb-8">로그인</h4>
+              <VAlert color="primary" variant="tonal" class="mb-4">
+                <p class="text-sm mb-2">
+                  Admin Email: <strong>admin@demo.com</strong> / Pass:
+                  <strong>admin</strong>
+                </p>
+                <p class="text-sm mb-0">
+                  Client Email: <strong>client@demo.com</strong> / Pass:
+                  <strong>client</strong>
+                </p>
+              </VAlert>
+              <VForm ref="refVForm" @submit.prevent="onSubmit">
+                <VRow>
+                  <!-- companycode -->
+                  <VCol cols="12">
+                    <AppTextField
+                      v-model="credentials.companycode"
+                      autofocus
+                      label="회사코드"
+                      type="text"
+                      placeholder="회사코드 입력"
+                      prepend-inner-icon="tabler-building-community"
+                      clearable
+                      @keydown.esc="onClear1"
+                    />
+                  </VCol>
+                  <!-- userid -->
+                  <VCol cols="12">
+                    <AppTextField
+                      v-model="credentials.userid"
+                      label="아이디 입력"
+                      type="text"
+                      placeholder="아이디 입력"
+                      prepend-inner-icon="tabler-user"
+                      clearable
+                      @keydown.esc="onClear2"
+                    />
+                  </VCol>
+                  <!-- password -->
+                  <VCol cols="12">
+                    <AppTextField
+                      v-model="credentials.password"
+                      label="비밀번호"
+                      placeholder="비밀번호 입력"
+                      :type="isPasswordVisible ? 'text' : 'password'"
+                      autocomplete="password"
+                      prepend-inner-icon="tabler-lock"
+                      clearable
+                      @keydown.esc="onClear3"
+                      :append-inner-icon="
+                        isPasswordVisible ? 'tabler-eye-off' : 'tabler-eye'
+                      "
+                      @click:append-inner="
+                        isPasswordVisible = !isPasswordVisible
+                      "
+                    />
+                  </VCol>
+                  <VCol cols="12">
+                    <!-- remember me checkbox -->
+                    <div
+                      class="d-flex align-center justify-space-between flex-wrap"
+                    >
+                      <VCheckbox v-model="rememberMe" label="아이디 저장" />
 
-              <!-- auth providers -->
-              <VCol cols="12" class="text-center">
-                <AuthProvider />
-              </VCol>
-            </VRow>
-          </VForm>
-        </VCardText>
-      </VCard>
-    </VCol>
-  </VRow>
+                      <RouterLink
+                        class="text-primary ms-2 mb-1"
+                        :to="{
+                          name: 'pages-authentication-forgot-password-v1',
+                        }"
+                      >
+                        비밀번호 재설정
+                      </RouterLink>
+                    </div>
+                  </VCol>
+                  <VCol cols="12">
+                    <!-- login button -->
+                    <VBtn block type="submit"> 로그인</VBtn>
+                  </VCol>
+                  <!-- create account -->
+                  <VCol cols="12" class="text-body-2 text-center">
+                    <span class="d-inline-block"> 관리자에게 계정요청 </span>
+                    <RouterLink
+                      class="text-primary ms-1 d-inline-block text-body-2"
+                      :to="{ name: 'pages-authentication-register-v1' }"
+                    >
+                      바로가기
+                    </RouterLink>
+                  </VCol>
+                </VRow>
+              </VForm>
+            </VCardText>
+          </VCard>
+        </VCol>
+      </VRow>
+      <div class="d-flex align-center justify-center footer">
+        <span class="d-flex align-center text-body-2">
+          &copy;
+          {{ new Date().getFullYear() }}
+          명인제약 by UBCare All Right Reserved.
+        </span>
+      </div>
+    </div>
+  </div>
 </template>
 
 <style lang="scss">
 @use "@core/scss/template/pages/page-auth";
+.text-h2 {
+  font-weight: 700;
+}
+.text-h5 {
+  font-weight: 700;
+}
+.app-logo-desk {
+  display: flex;
+  height: 64px;
+  width: fit-content;
+  gap: 12px;
+  .app-logo-title {
+    font-size: 44px;
+  }
+  img {
+    height: 64px;
+  }
+  .v-responsive__content {
+    flex: none;
+  }
+}
+@media (max-width: 1280px) {
+  .v-card-text {
+    background: none;
+  }
+  .v-card--variant-elevated {
+    background: none;
+    box-shadow: none;
+  }
+  .v-field__overlay {
+    background: #fff !important;
+  }
+  .app-logo {
+    height: 40px;
+    gap: 8px;
+    .app-logo-title {
+      font-size: 28px;
+    }
+    img {
+      height: 40px;
+    }
+  }
+  .footer {
+    width: 100%;
+  }
+  .background-image {
+    background-color: #fff;
+  }
+}
+@media (min-width: 1281px) {
+  .position-relative {
+    width: 1000px;
+  }
+  .footer {
+    margin-top: 24px;
+  }
+  .background-image {
+    background-color: var(--color-primary-primary-100);
+  }
+  .match-height {
+    > .v-col-12 {
+      padding: 0 !important;
+    }
+  }
+}
+.background-side {
+  background: url("@/assets/images/front-pages/backgrounds/login-bg-02.png"),
+    linear-gradient(151deg, black 0%, #00348b 100%);
+  background-size: cover;
+  background-position: center center;
+  background-repeat: no-repeat;
+  height: 100vh;
+  width: 100vw;
+  max-width: 100%;
+  overflow: hidden;
+}
+
+.auth-card {
+  border-radius: 20px;
+}
+.color-red {
+  color: #e60012;
+}
+.color-blue {
+  color: #113264;
+}
 </style>
